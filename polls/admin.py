9abdeec02795b905a1225e7c9f9cc3070332394c
lpatmo <linda.peng@alumni.duke.edu>
2016-08-22 23:59:34 -0400

    latest - added styles

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,21 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+
