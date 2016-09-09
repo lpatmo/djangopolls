e2c6e8eca03cf364a80c809aba702c743be3c613
lpatmo <linda.peng@alumni.duke.edu>
2016-09-08 19:49:36 -0700

    added login/logout and changed home page

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,10 +53,16 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'mysite.urls'
 
+# Add static folder to STATIC_DIRS
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"], #[os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +74,8 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGIN_REDIRECT_URL = '/' # It means home view
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
@@ -100,6 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
 
 
 # Internationalization
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth import views
+from log.forms import LoginForm
+
+
 urlpatterns = [
+	#url(r'', include('polls.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'', include('log.urls')),  
+    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
+    url(r'^logout/$', views.logout, {'next_page': '/login'}),  
 ]
+
+LOGIN_REDIRECT_URL = '/' # It means home view
